a3c3310e9a1a30381f4df4f7bfcb9014ebe037ab
whats2000 <whats2000mc@gmail.com>
2022-11-03 11:35:16 +0800

    Update navbar JavaScript

--- scripts/index.html.js
+++ scripts/index.html.js
@@ -1,28 +1,22 @@
-// Script from https://www.w3schools.com/HOWTO/howto_html_include.asp
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include-html");
-        if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute("include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
-        }
-    }
+/* Script from https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
+var prev_scroll_pos = window.pageYOffset;
+
+function navbar_scroll() {
+    let current_scroll_pos = window.pageYOffset;
+    if (current_scroll_pos > 1)
+        document.getElementById("navbar").classList.add("nav-bg-black");
+    else
+        document.getElementById("navbar").classList.remove("nav-bg-black");
+
+    if (current_scroll_pos < document.getElementById("carousel-travel").offsetHeight)
+        return;
+
+    if (prev_scroll_pos > current_scroll_pos)
+        document.getElementById("navbar").style.top = "0";
+    else
+        document.getElementById("navbar").style.top = `-${document.getElementById("navbar").offsetHeight}px`;
+
+    prev_scroll_pos = current_scroll_pos;
 }
+
+window.onscroll = navbar_scroll;

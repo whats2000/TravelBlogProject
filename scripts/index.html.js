90997827fd1cba1f51a1f56e2308bf7d62d4c53a
whats2000 <whats2000mc@gmail.com>
2022-11-12 19:54:22 +0800

    Change the homepage id to 'index-name'

--- scripts/index.html.js
+++ scripts/index.html.js
@@ -4,17 +4,17 @@ var prev_scroll_pos = window.pageYOffset;
 function navbar_scroll() {
     let current_scroll_pos = window.pageYOffset;
     if (current_scroll_pos > 1)
-        document.getElementById("navbar").classList.add("nav-bg-black");
+        document.getElementById("index-navbar").classList.add("nav-bg-black");
     else
-        document.getElementById("navbar").classList.remove("nav-bg-black");
+        document.getElementById("index-navbar").classList.remove("nav-bg-black");
 
-    if (current_scroll_pos < document.getElementById("carousel-travel").offsetHeight)
+    if (current_scroll_pos < document.getElementById("index-carousel-travel").offsetHeight)
         return;
 
     if (prev_scroll_pos > current_scroll_pos)
-        document.getElementById("navbar").style.top = "0";
+        document.getElementById("index-navbar").style.top = "0";
     else
-        document.getElementById("navbar").style.top = `-${document.getElementById("navbar").offsetHeight}px`;
+        document.getElementById("index-navbar").style.top = `-${document.getElementById("index-navbar").offsetHeight}px`;
 
     prev_scroll_pos = current_scroll_pos;
 }

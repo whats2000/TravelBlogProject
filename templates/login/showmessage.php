b62e270ff209a2fb09ecc9b75cf2e32a6bb0c865
whats2000 <whats2000mc@gmail.com>
2022-12-02 14:09:54 +0800

    Update showmessage.php

--- templates/login/showmessage.php
+++ templates/login/showmessage.php
@@ -3,15 +3,16 @@ if (isset($_SESSION["show_message"])) { ?>
 <div class="modal fade" id="modal-show-message" aria-hidden="true" aria-labelledby="modal-show-message-label"
     tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content rounded-4 shadow">
-            <div class="modal-body p-4 text-center">
-                <h5 class="mb-0" id="modal-show-message-label">
-                    <strong><?=$_SESSION["show_message"]?></strong>
-                </h5>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title fw-bold" id="modal-show-message-label">Notification</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-footer px-4 pb-4 flex-column border-top-0">
-                <button type="button" class="btn btn-md btn-secondary w-100 mx-0" data-bs-dismiss="modal">I get
-                    it</button>
+            <div class="modal-body">
+                <p><?=$_SESSION["show_message"]?></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>

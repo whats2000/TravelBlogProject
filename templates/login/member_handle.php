dbdaec0b7a29869c00f08a946adf3b9335461efe
whats2000 <whats2000mc@gmail.com>
2022-12-15 11:49:52 +0800

    Upload final part of post

--- templates/login/member_handle.php
+++ templates/login/member_handle.php
@@ -65,50 +65,33 @@ if (@$_POST["method"] == "login") {
         $return_msg = "Invalid email format";
     } else {
         $email = $sql_link->quote($email);
-
-        $name = $_POST["name"];
-        $sql = "SELECT * FROM `user` WHERE `email` = $email";
-        $result = $sql_link->query($sql);
         $email = test_input($_POST["email"]);
         $name = $_POST["name"];
 
-        $sql = "SELECT * FROM `user` WHERE `email` = '$email'";
+        $sql = "SELECT * FROM `user` WHERE `email` = $email";
         $result = $sql_link->query($sql);
 
         if ($result) {
             $num = $result->rowCount();
             if ($num == 0) {
-                date_default_timezone_set('Asia/Taipei');
-                $current_time = date('Y-m-d h:i:s');
-                $name = $sql_link->quote($name);
-                $password = $sql_link->quote($password);
-                $sql = "INSERT INTO `user` (`name`, `email`, `password`, `create_at`) 
-                    VALUES ($name, '$email', $password, '$current_time')";
-                if ($result) {
-                    $num = $result->rowCount();
-                    if ($num == 0) {
-                        date_default_timezone_set('Asia/Taipei');
-                        $current_time = date('Y-m-d h:i:s');
-                        $sql = "INSERT INTO `user` (`name`, `email`, `password`, `create_at`) 
-                    VALUES ('$name', '$email', '$password', '$current_time')";
-
-                        if ($sql_link->exec($sql)) {
-                            $return_msg = "Sign up successfully";
-                            // 將會員名稱存入session
-                            $_SESSION["user"] = ["name" => $name,
-                                                 "email" => $email,
-                                                 "icon" => "",
-                                                 "permission" => "user"];
-                        } else {
-                            $return_msg = "Fail to write data to database";
-                        }
-                    } else {
-                        $return_msg = "This email have already sign up, please use other email";
-                    }
+                $return_msg = "Can not find membership for this user";
+            } else {
+                $row = $result->fetch();
+                if ($row["password"] == $password) {
+                    // 將會員名稱存入session
+                    $_SESSION["user"] = [
+                        "name" => $row["name"],
+                        "email" => $row["email"],
+                        "icon" => $row["icon"],
+                        "permission" => $row["permission"]
+                    ];
+                    $return_msg = "Login successfully";
                 } else {
-                    $return_msg = "Fail to fetch data from database";
+                    $return_msg = "Password or email is incorrect";
                 }
             }
+        } else {
+            $return_msg = "Fail to fetch data from database";
         }
     }
 }

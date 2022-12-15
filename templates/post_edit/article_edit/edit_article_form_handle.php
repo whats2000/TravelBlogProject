14b294b03c6de683bd7220125ca92a94b5899906
hsiangm6--筆電 <hsiangm6@gmail.com>
2022-12-15 11:10:24 +0800

    Add PDO->quote to edit_article_form_handle.php

--- templates/post_edit/article_edit/edit_article_form_handle.php
+++ templates/post_edit/article_edit/edit_article_form_handle.php
@@ -3,17 +3,17 @@
 session_start();
 
 if (!isset($_SESSION["user"])) { ?>
-<script>
-window.location.href = '../../index.php';
-</script>
+    <script>
+        window.location.href = '../../index.php';
+    </script>
 <?php exit();
 }
 
 if (!isset($_POST["article-id"])) {
     $_SESSION["show_message"] = "Undefine article id"; ?>
-<script>
-window.location.href = '../../index.php';
-</script>
+    <script>
+        window.location.href = '../../index.php';
+    </script>
 <?php exit();
 }
 
@@ -42,6 +42,7 @@ if (isset($_POST["save-edit-article"])) {
     $title = $_POST["article-title"];
     $display = $_POST["article-display-edit"];
     $edit_time = date("Y-m-d H:i:s");
+    $title = $sql_link->quote($title);
 
     $url = "../../post_edit/post_edit_handle.php";
     //$articl_id_string = implode("", $_POST["article-id"]);
@@ -63,11 +64,11 @@ if (isset($_POST["save-edit-article"])) {
             $dir = "static/images/blog_post/article/";
             if (move_uploaded_file($file_tmp_name, "../../../" . $dir . $article_id_image_name)) {
                 $sql = "UPDATE `article`
-                        SET `title`='$title', `picture`='$article_id_image_name', `description`='$description', 
+                        SET `title`=$title, `picture`='$article_id_image_name', `description`='$description', 
                         `display`='$display', `edit_time`='$edit_time'
                         WHERE `id`='$article_id'";
 
-                $edit_article = $sql_link-> exec($sql);
+                $edit_article = $sql_link->exec($sql);
             }
             $url = "../../post_edit/post_edit_handle.php";
         } else {
@@ -75,8 +76,9 @@ if (isset($_POST["save-edit-article"])) {
             $return_msg = "Fail to fetch article data from database";
         }
     } else {
+        $title = $sql_link->quote($title);
         $sql = "UPDATE `article`
-            SET `title`='$title', `description`='$description', `display`='$display', `edit_time`='$edit_time'
+            SET `title`=$title, `description`='$description', `display`='$display', `edit_time`='$edit_time'
             WHERE `id`='$article_id'";
         $edit_article = $sql_link->exec($sql);
 
@@ -91,9 +93,9 @@ if (isset($_POST["save-edit-article"])) {
     if ($return_msg != "") {
         $_SESSION["show_message"] = $return_msg;
     } ?>
-<script>
-window.location.href = '<?= $url ?>';
-</script>
+    <script>
+        window.location.href = '<?= $url ?>';
+    </script>
 <?php }
 
 exit(); ?>
